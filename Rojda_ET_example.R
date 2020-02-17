9d6cbb8986cff437468a90684155d5352505532c
rcirone <rcirone@iastate.edu>
2020-02-17 15:15:39 -0600

    Create Rojda_ET_example.R
    The file that Rojda made to run the example in the Penman Monteith formulation

---
+++ Rojda_ET_example.R
@@ -0,0 +1,27 @@
+##17 February 2020
+##Rojda
+##"Evapotranspiration" package usage exercise
+
+
+##First install the "Evapotranspiration" package
+
+library(Evapotranspiration) ##  Hit the "Run" and go to the right bottom window and click the "Packages" segment to check if "Evapotranspiration"  is there or not. 
+##If it did not work somehow, please try the "install" segment just below the "packages" segment. Click "install" and type "Evapotranspiration" in the Packages. 
+##Now please click the "Evapotranspiration" in the Packages segment on the left bottom window. And find the "ET.PenmanMonteith". 
+##Click "ET.PenmanMonteith" and please read the document. At the end of the text, there is an example of data that you can run.
+
+##Example
+
+data("processeddata")
+data("constants")
+
+results <- ET.PenmanMonteith(data, constants, ts="daily", solar="sunshine hours", 
+                             wind="yes", crop = "short", message="yes", AdditionalStats="yes", save.csv="yes") 
+
+
+##try to make simple plot
+
+plot(results$ET.Daily)
+
+##Your Turn:
+##Now please make some changes to the graph. You can use the previous R script in the Announcement in Canvas.
