bc8f1e41f78c7c2d1910bbbdfda021864afabbfa
JoshBendorf <JoshBendorf@users.noreply.github.com>
2020-03-04 13:34:08 -0600

    Josh

--- OurFirstShiny/app.R
+++ OurFirstShiny/app.R
@@ -9,18 +9,14 @@
 
 library(shiny)
 library(tidyverse)
-library(scales)
-
-
 
 
 #--read in the data
 
-dat<- read.csv("C:/Users/myAdmins/Desktop/Rshiny_flux/data_ET-for-shiny.csv")
-
+dat <- read.csv("data/tidy/data_ET-for-shiny.csv")
 
 #--create the drop down menu values
-dd_month <- dat %>% select(month_id) %>% pull() %>% unique() 
+dd_year <- dat %>% select(year_id) %>% pull() %>% unique()
 
 ui <- fluidPage(
     
@@ -30,9 +26,9 @@ ui <- fluidPage(
     # Sidebar with a slider input for number of bins 
     sidebarLayout(
         sidebarPanel(
-            selectInput("mymonth",
-                        "Month:",
-                        choices = dd_month)
+            selectInput("myyear",
+                        "Year:",
+                        choices = dd_year)
         ),
         
         # Show a plot of the generated distribution
@@ -44,27 +40,26 @@ ui <- fluidPage(
 
 # Define server logic required to draw a histogram
 server <- function(input, output) {
-
-    ##--build reactive dataset, changes year highlight
-    liq_dat <- reactive({
-        dat %>% 
-            mutate(color_id = ifelse(month_id == input$mymonth, "selected month", "no"))
-      #mutate(color_id = ifelse(month_id == input$myyear, "selected year", "no"))
-    })
-    
-    output$etPlot <- renderPlot({
-        # generate bins based on input$bins from ui.R
-        ggplot(data = liq_dat(),
-               aes(x =month_id,
-                   y = ET_monthly)) + 
-            geom_point(aes(color = color_id), size = 5) +
-            scale_color_manual(values = c("selected year" = "red",
-                                          "no" = "gray80")) +
-            #labs(x = "Maximum Monthly Temp (degC)",
-            #     y = "Montly Total Evapotranspiration (ET; mm)",
-            #     color = NULL)
-            theme_bw()
-    })
+  
+  ##--build reactive dataset, changes year highlight
+  liq_dat <- reactive({
+    dat %>% 
+      mutate(color_id = ifelse(year_id == input$myyear, "selected year", "no"))
+  })
+  
+  output$etPlot <- renderPlot({
+    # generate bins based on input$bins from ui.R
+    ggplot(data = liq_dat(),
+           aes(x = Tmax,
+               y = ET_monthly)) + 
+      geom_point(aes(color = color_id), size = 5) +
+      scale_color_manual(values = c("selected year" = "red",
+                                    "no" = "gray80")) +
+      #labs(x = "Maximum Monthly Temp (degC)",
+      #     y = "Montly Total Evapotranspiration (ET; mm)",
+      #     color = NULL)
+      theme_bw()
+  })
 }
 
 # Run the application 
