2e3236b8640ccf22a3ca99eda79a51f04aa8f876
JoshBendorf <jbendorf@iastate.edu>
2020-04-08 12:15:33 -0500

    Changes from class - April 8

--- Shiny_Farmer/app.R
+++ Shiny_Farmer/app.R
@@ -28,6 +28,7 @@ library(lubridate)
 
 # dat <- read_csv("data/tidy/ET_miscanthus_SABR.csv") %>% 
 #   mutate(month_id = month(Month, label = T))
+#
 
 
 et <- read_csv("../data/tidy/ET_miscanthus_SABR.csv") %>% 
