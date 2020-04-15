1e9d96e4fcfb18a0b1550d627aeffdda720b87e1
vanichols <vnichols@iastate.edu>
2020-04-14 20:37:36 -0500

    update Shiny_Farmer App w/TJ's requests

--- Shiny_Farmer/app.R
+++ Shiny_Farmer/app.R
@@ -15,27 +15,27 @@ library(lubridate)
 library(shinythemes)
 
 #--read in the data for trouble-shooting
-
-# et <- 
-#   read_csv("data/tidy/data_et-sabr-miscan.csv") %>% 
+# 
+# et <-
+#   read_csv("data/tidy/data_et-sabr-miscan.csv") %>%
 #   mutate(date = mdy(Month),
 #          month_id = month(date),
-#          doy = DOY) %>% 
+#          doy = DOY) %>%
 #   select(year_id, month_id, doy, ET_daily)
-#   
-# wx <- read_csv("data/tidy/data_ames-wea-2019.csv") %>% 
+# 
+# wx <- read_csv("data/tidy/data_ames-wea-2019.csv") %>%
 #   mutate(year_id = year(date))
 
 
 
-et <- 
-  read_csv("../data/tidy/data_et-sabr-miscan.csv") %>% 
+et <-
+  read_csv("../data/tidy/data_et-sabr-miscan.csv") %>%
   mutate(date = mdy(Month),
          month_id = month(date),
-         doy = DOY) %>% 
+         doy = DOY) %>%
   select(year_id, month_id, doy, ET_daily)
 
-wx <- read_csv("../data/tidy/data_ames-wea-2019.csv") %>% 
+wx <- read_csv("../data/tidy/data_ames-wea-2019.csv") %>%
   mutate(year_id = year(date))
 
 
@@ -46,36 +46,54 @@ dat <-
   mutate(month_lab = month(month_id, label = T))
 
 # for trouble shooting ----------------------------------------------------
-
-     
-dat.fake <- 
-  dat %>%
-  filter(month_lab == "Jun",
-         year_id == 2019) %>% 
-  mutate(cc = as.numeric(0.5)) %>% 
-  mutate(ET_coef = ET_daily * cc,
-         net_mm = -ET_coef + precip_mm,
-         cum_mm = cumsum(net_mm),
-         paw = as.numeric(15) + cum_mm)
-
-    
-dat.fake %>% 
-  ggplot(aes(date, paw)) + 
-  geom_point() + 
-  scale_x_date(date_labels = "%b %d")
+# 
+#      
+# dat.fake <- 
+#   dat %>%
+#   filter(month_lab == "Jun",
+#          year_id == 2019) %>% 
+#   mutate(cc = as.numeric(0.5)) %>% 
+#   mutate(PET = ET_daily,
+#          ET = PET * cc,
+#          net_mm = -ET + precip_mm,
+#          cum_mm = cumsum(net_mm),
+#          smois_pct = as.numeric(50) + cum_mm,
+#          smois_pct = ifelse(smois_pct < 0, 0, 
+#                             ifelse(smois_pct > 100, 100, smois_pct)))
+# 
+# dat %>%
+#   filter(month_lab == input$tjmonth,
+#          year_id == input$tjyear) %>% 
+#   mutate(cc = as.numeric(input$tjcc)) %>% 
+#   mutate(
+#     PET = ET_daily,
+#     ET = PET * cc,
+#     net_mm = -ET + precip_mm,
+#     cum_mm = cumsum(net_mm),
+#     smois_pct = as.numeric(input$tjsmois) + cum_mm,
+#     smois_pct = ifelse(smois_pct < 0, 0, 
+#                        ifelse(smois_pct > 100, 100, smois_pct)),
+#     smois_color = ifelse(smois_pct < as.numeric(input$tjpwp), "bad", "good"))
+# 
+#     
+# dat.fake %>% 
+#   ggplot(aes(date, smois_pct)) + 
+#   geom_point() + 
+#   scale_x_date(date_labels = "%b %d")
 
 #--create the drop down menu values
 dd_year <- dat %>% select(year_id) %>% pull() %>% unique()
 dd_month <- dat %>% select(month_lab) %>% pull() %>% unique()
-dd_paw <- seq(1, 15, 1)
+dd_smois <- seq(0, 100, 5)
+
 
 ui <- fluidPage(
   theme = shinytheme("cosmo"),
   # Application title
-  navbarPage("Fun Class Projects"),
+  navbarPage("Agron518 Class Projects"),
   
   tabsetPanel(tabPanel(
-    "Evapotranspiration and Crop Growth",
+    "Evapotranspiration Effects on Soil Water",
     
     fluidRow(
       column(6,
@@ -121,20 +139,19 @@ ui <- fluidPage(
        column(
          2,
          selectInput(
-           inputId = "tjpaw",
-           label = h4("Starting PAW (mm):"),
-           selected = 15,
-           choices = dd_paw
-         )
-       ),
+           "tjsmois",
+           label = h3("Starting Soil Water (vol%):"),
+           selected = 50,
+           choices = dd_smois)
+         ),
         column(
           3,
           sliderInput(
-            "tjpawcut",
-            label = h3("Plant Wilting Point (mm?)"),
-            min = -15,
-            max = 0,
-            value = -5,
+            "tjpwp",
+            label = h3("Plant Wilting Point (vol%)"),
+            min = 0,
+            max = 50,
+            value = 15,
             step = 1
             
           )
@@ -149,11 +166,18 @@ ui <- fluidPage(
           tags$img(src = "kc-image.png", height = 200, width = 300, align = "center")
         ),
         column(
+          2,
+          #offset = 1,
+          h3("Soil Water Visual"),
+          tags$img(src = "pwp-image.gif", height = 200, width = 200, align = "center")
+        ),
+        column(
           3,
-          offset = 2,
+          #offset = 1,
           h3("Plant Wilting Point Help"),
-          tags$img(src = "pwp-image.gif", height = 200, width = 300, align = "center")
+          tags$img(src = "soil water.png", height = 200, width = 300, align = "center")
         )
+        
       )
       
     )
@@ -175,29 +199,41 @@ server <- function(input, output) {
       filter(month_lab == input$tjmonth,
              year_id == input$tjyear) %>% 
       mutate(cc = as.numeric(input$tjcc)) %>% 
-      mutate(ET_coef = ET_daily * cc,
-             net_mm = -ET_coef + precip_mm,
-             cum_mm = cumsum(net_mm),
-             paw = as.numeric(input$tjpaw) + cum_mm,
-             paw_color = ifelse(paw < as.numeric(input$tjpawcut), "bad", "good"))
+      mutate(
+        PET = ET_daily,
+        ET = PET * cc,
+        net_mm = -ET + precip_mm,
+        cum_mm = cumsum(net_mm),
+        smois_pct = as.numeric(input$tjsmois) + cum_mm,
+        smois_pct = ifelse(smois_pct < 0, 0, 
+                           ifelse(smois_pct > 100, 100, smois_pct)),
+        smois_color = ifelse(smois_pct < as.numeric(input$tjpwp), "bad", "good"))
     
   })
   
   
   output$tjPlot1 <- renderPlot({
   
-      ggplot(data = liq_tj1(),
-           aes(x = date,
-               y = ET_coef)) +
-      geom_line(color = "purple", size = 2) + 
-      geom_line(aes(y = ET_daily),
-                color = "gray80") +
+    liq_tj1() %>% 
+      select(year_id, month_id, doy, date, month_lab, PET, ET) %>% 
+      pivot_longer(PET:ET) %>% 
+      mutate(name = factor(name, levels = c("PET", "ET"))) %>% 
+      ggplot(aes(x = date,
+               y = value, 
+               color = name)) + 
+      geom_point(size = 5) +
+      geom_line() +
+      scale_color_manual(values = c("PET" = "gray80",
+                                    "ET" = "purple")) +
       theme_bw() + 
       scale_x_date(date_labels = "%b %d") +
       theme(axis.title = element_text(size = rel(1.5)),
-            axis.text = element_text(size = rel(1.2))) +
+            axis.text = element_text(size = rel(1.2)),
+            legend.position = "top",
+            legend.direction = "horizontal") +
       labs(x = NULL,
-           y = "Daily Evapotranspiration (mm)")
+           color = NULL,
+           y = "Daily (Potential) Evapotranspiration (mm)")
   })
 
   
@@ -205,17 +241,17 @@ server <- function(input, output) {
     
      
      ggplot(data = liq_tj1(),
-            aes(date, paw)) + 
-       geom_hline(yintercept = input$tjpawcut,
+            aes(date, smois_pct)) + 
+       geom_hline(yintercept = input$tjpwp,
                   linetype = "dashed",
                   color = "black") + 
        geom_line(color = "gray", size = 1, linetype = "dashed") +
-       geom_point(aes(color = paw_color), size = 5) +
+       geom_point(aes(color = smois_color), size = 5) +
        scale_x_date(date_labels = "%b %d") + 
        theme_bw() + 
        guides(color = F) + 
        scale_color_manual(values = c("bad" = "red", 
-                                     "good" = "black")) +
+                                     "good" = "blue")) +
        theme(axis.title = element_text(size = rel(1.5)),
              axis.text = element_text(size = rel(1.2))) +
        labs(x = NULL,
